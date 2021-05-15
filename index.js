3f799a9b64c62b0e90fe86b5b3a0275b6771ae8c
VanshikaGahlawat <gahlawatv23@gmail.com>
2021-05-16 00:29:20 +0530

    end and restart game

--- index.js
+++ index.js
@@ -12,43 +12,63 @@ const game ={
     player:'X'
 }
 
-function checkSeq(seq){
+function endGame(winner){
+    if (winner) {
+        alert('Game Over | Winner: '+ winner);
+    }
+    else {
+        alert('Game Over | Match Draw');
+    }
+    game.state = end;
+
+}
+
+function restartGame(){
+    game.state = start;
+    game.moves= 0;
+    
+    if( Math.random() > 0.5) game.player= 'X'
+    else game.player='O'
+
+    Array.from(document.getElementsByTagName('td')).forEach( cell => cell.textContent= '')
+ }
+
+function checkSeq(seqArray){
     const winningseq= game.player+game.player+game.player;
-    if( seq === winningseq) alert('Game Over | Winner: ' + game.player);
+    if( seqArray.map( i => i.textContent).join('') === winningseq) {
+        endGame(game.player);
+    }
     else return;
 }
 
 function isRowCaptured(presentRow){
-    const tblrow= Array.from(gameTable.children[0].children[presentRow-1].children)
-    const rowSeq= tblrow.map(item => item.textContent).join('');
+    const rowSeq= Array.from(gameTable.children[0].children[presentRow-1].children)
     checkSeq(rowSeq);
 }
 
 function isColCaptured(presentCol){
-    const tblcol= [ 
+    const colSeq= [ 
         gameTable.children[0].children[0].children[presentCol-1],
         gameTable.children[0].children[1].children[presentCol-1],
         gameTable.children[0].children[2].children[presentCol-1]
     ]
-    const colSeq= tblcol.map(item => item.textContent).join('');
     checkSeq(colSeq);
 }
 
 function isDiagCaptured(row, col){
-    if (row !== col && (row+ col) !== 4) return;
+    if (row !== col && (row + col) !== 4) return;
     const diag1= [
         gameTable.children[0].children[0].children[0],
         gameTable.children[0].children[1].children[1],
         gameTable.children[0].children[2].children[2]
-    ].map(item => item.textContent).join('');
-    console.log(diag1)
+    ]
     checkSeq(diag1);
 
     const diag2= [
         gameTable.children[0].children[0].children[2],
         gameTable.children[0].children[1].children[1],
         gameTable.children[0].children[2].children[0]
-    ].map( item => item.textContent).join('');
+    ]
     checkSeq(diag2);
 }
 
@@ -56,13 +76,18 @@ function nextTurn(){
     game.moves++;
     if (game.player === "X") game.player= "O"
     else game.player="X"
-    turn.textContent = game.player;
 
-    if(game.moves === 9) alert("Game Over");
+    if(game.moves === 9) endGame();
+
+    turn.textContent = game.player;
 }
 
 function boxClicked(row, col){
-    const box= gameTable.children[0].children[row-1].children[col-1];
+    if (game.state === end) {
+        alert('Game Ended | Restart to play');
+        return
+    }
+    let box= gameTable.children[0].children[row-1].children[col-1];
     box.textContent = game.player;
     isRowCaptured(row);
     isColCaptured(col);
